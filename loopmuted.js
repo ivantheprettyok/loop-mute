6f6e21237e518f46f42c921018e4b7d958c8ed91
ivantheprettyok <80988895+ivantheprettyok@users.noreply.github.com>
2021-03-21 21:55:01 -0500

    Update loopmuted.js
    alt m hot key to un/mute

--- loopmuted.js
+++ loopmuted.js
@@ -1,18 +1,29 @@
-//alert("WORKING");
+//If the first thing on the page is a video
+if (document.body.firstChild instanceof HTMLVideoElement) {
+    //alert("VIDEO");
 
-//Get the first video
-var vid = document.getElementsByTagName("video")[0];
+    //Get the first video
+    var vid = document.getElementsByTagName("video")[0];
 
-//If theres a video
-if (vid !== undefined) {
-    //alert(vid);
-    
-    //If the video type matches -> loop and mute
-    t = vid.getElementsByTagName("source")[0].type;
-    if ((t == "video/webm") || (t == "video/ogg")){
-        vid.setAttribute("loop", "loop");
-        vid.muted = true;
-    }
-}
+    //If theres a video.  Double check.
+    if (vid !== undefined) {
+        //If the video type matches -> loop and mute
+        t = vid.getElementsByTagName("source")[0].type;
+        if ((t == "video/webm") || (t == "video/ogg") || (t == "video/mp4")){
+            vid.setAttribute("loop", "loop");
+            vid.muted = true;
+        }
 
+        //mute hotkey
+        document.addEventListener('keydown', logKey);
+
+        function logKey(e) {
+            if (e.altKey && (e.code == "KeyM")){
+                vid.muted = !vid.muted;
+                //alert("MUTE");
+            }//if
+        }//logkey
+
+    }//If video present
+}//If first thing is video
 void 0;
